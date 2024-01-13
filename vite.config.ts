0382362b7e2caec79b61bc2cb0ac52c148481547
jakubk791 <jakubkarolmazur@gmail.com>
2024-01-13 18:35:58 +0100

    small config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()]
+    base: "./",
+    build: {
+        target: "esnext"
+    },
+    plugins: [
+        svelte({
+            preprocess: vitePreprocess({})
+        })
+    ]
 });
